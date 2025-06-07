6a1fe2374842d90509de2b5d9aa4b51cb8284354
Syskey-pixel <syskeygt@gmail.com>
2025-06-07 20:08:23 +0700

    Username.cpp

---
+++ username.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+int main(){
+	std:: cout<<"*wrong input=wrong output okay?"<<'\n';
+	std::cout<<"*upper case class:"<<'\n';
+    char c;
+	std::cin>>c;
+	int n;
+	std::string initial;
+	switch(c){
+	case 'A':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+	case 'B':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		n+=39;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+	case 'C':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		n+=79;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+	case 'D':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		n+=119;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+	case 'E':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		n+=159;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+	case 'F':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		n+=199;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+	case 'G':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		n+=234;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+	case 'H':
+		std::cout<<"enter a person's absence number"<<'\n';
+		std::cin>>n;
+		n+=258;
+		std:: cout<<"enter person's name or initial"<<'\n';
+		
+		std::cin>>(std::cin,initial);	std::cout<<"username:"<<"ASAT2025"<<"L7"<<c<<n<<initial<<'\n';
+		std::cout<<"if absence number less than 10 it might be wrong";
+		break;
+		}
+	}
